bd93891fbfefd70d9ad52c23599044d1b8de71af
minzero31 <77minyoung77@gmail.com>
2024-10-30 20:58:21 +0900

    기본 구조 세팅

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.thenightog.aiit.applyevent.thenightofaiitapplyevent {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.thenightog.aiit.applyevent.thenightofaiitapplyevent to javafx.fxml;
-    exports com.thenightog.aiit.applyevent.thenightofaiitapplyevent;
-}
